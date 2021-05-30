00235212ad6ffd3c1274af6db40fcbfbcd69cb80
bmosurek <68894569+bmosurek@users.noreply.github.com>
2021-05-30 17:49:47 +1000

    Add: script.js accortion-with bug

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-  'use strict';
+  ('use strict');
 
   const select = {
     templateOf: {
@@ -45,21 +45,98 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    },
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    menuProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.menuProduct).innerHTML
+    ),
   };
 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /*add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+    /* adding accordion */
+    initAccordion() {
+      const thisProduct = this;
+      /* find the clicable trigger (the element that should react to clicking)*/
+      const clickableTrigger = document.querySelector(
+        select.menuProduct.clickable
+      );
+      /* czy aby użyć article.querySelector(select.menuProduct.clickable); 
+      muszę najpierw zadeklarować stałą article? */
+      /* START: add event listener to clickable trigger on event 'click'*/
+      clickableTrigger.addEventListener('click', function (event) {
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product */
+        const activeProduct = document.querySelector(
+          select.templateOf.menuProduct,
+          classNames.menuProduct.wrapperActive
+        );
+        console.log('active product', activeProduct);
+
+        /* if there is active product and it's not thisProduct.element, remove active class from  it */
+        if (thisProduct.element != activeProduct) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle  active class on thisProduct.element */
+        thisProduct.element.classList.toggle(
+          classNames.menuProduct.wrapperActive
+        );
+      });
+    }
+  }
+
   const app = {
-    init: function(){
+    initMenu: function () {
+      const thisApp = this;
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource; // czy ma znaczenie, że obiekt dataSource jest w innym pliku?
+      //co jeśli zmieni się ścieżka dostępu dla data.js?
+    },
+
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
